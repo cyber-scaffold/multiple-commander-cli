bea2f70606de57182aa1526b8f45ea1a882311b5
yushicheng <1542874601@qq.com>
2021-08-28 18:23:53 +0800

    temp:临时节点标记 星期六 晚上 6:23:52 八月 28日 2021 创建了 src/actions/test_action1.js 创建了 src/actions/test_action2.js 修改了 package.json 修改了 src/main.js 删除了 src/actions/index.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,26 @@
-import { program } from "commander";
+import {program} from "commander";
 import {name,version} from "@@/package.json";
+
+import {test_action_argument1,test_action_option1,test_action1} from "@/actions/test_action1";
+import {test_action_argument2,test_action_option2,test_action2} from "@/actions/test_action2";
 
 program
   .usage(name)
   .version(version);
 
 program
-  .command("")
-  .description("")
-  .requiredOption("-e, --env <string>", "环境变量",(value)=>{
-    const allow_values=["test","prod"];
-    if(allow_values.includes(value)){
-      process.env.TRANSFORM_ENV=value;
-    }else{
-      console.log("参数env的值",value,"应该为",allow_values.join(","));
-      process.exit(0);
-    };
-  })
-  .action(require("@/actions/index").default);
+  .command("test1")
+  .description("多类型测试命令1")
+  .addArgument(test_action_argument1)
+  .addOption(test_action_option1)
+  .action(test_action1);
+
+program
+  .command("test2")
+  .description("多类型测试命令2")
+  .addArgument(test_action_argument2)
+  .addOption(test_action_option2)
+  .action(test_action2);
 
 program.parse();
 
