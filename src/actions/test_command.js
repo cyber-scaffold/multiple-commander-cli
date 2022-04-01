c47a6f37b94115a989b1c45231565e8ffde12641
yushicheng <1542874601@qq.com>
2022-04-01 12:00:12 +0800

    temp:临时节点标记 星期五 中午 12:00:12 四月 1日 2022 修改了 src/actions/test_command.js

--- src/actions/test_command.js
+++ src/actions/test_command.js
@@ -1,12 +1,9 @@
-import {Argument,Option} from "commander";
+// import {Argument,Option} from "commander";
 import load_config from "@/utils/load_config";
 
-export const test_command_argument=new Argument("<actions>","动作类型描述").choices(["init","info"]);
-export const test_command_option=new Option("-t,--test_option <string>").default("test_option_value");
+// export const test_command_argument=new Argument("<actions>","动作类型描述").choices(["init","info"]);
+// export const test_command_option=new Option("-t,--test_option <string>").default("test_option_value");
 
-/**
- * @description 用于将图片转换压缩变成低质量的jpg图片
- * **/
 export async function test_command(){
   console.log(await load_config());
 }
